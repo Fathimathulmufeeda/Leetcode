df7c5daa33c3514c17f33b11431aa40b1abc696b
Fathimathulmufeeda <fathimathulmufeedaa@gmail.com>
2026-08-03 10:10:38 +0530

    Time: 0 ms (100%), Space: 56 MB (6.12%) - LeetHub

--- 1281-subtract-the-product-and-sum-of-digits-of-an-integer/1281-subtract-the-product-and-sum-of-digits-of-an-integer.ts
+++ 1281-subtract-the-product-and-sum-of-digits-of-an-integer/1281-subtract-the-product-and-sum-of-digits-of-an-integer.ts
@@ -2,10 +2,11 @@ function subtractProductAndSum(n: number): number {
   let str=n.toString()
   let sum=0
   let pro=1
-  for(let i=0;i<str.length ;i++){
-      let digit=Number(str[i])
+  while(n>0){
+      let digit=n%10
       sum+=digit
       pro*=digit
+      n=Math.floor(n/10)
   }
   return pro-sum
 };
