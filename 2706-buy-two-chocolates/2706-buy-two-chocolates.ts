77cee621a96ca67b8328cd20dd35b00d630c48bc
Fathimathulmufeeda <fathimathulmufeedaa@gmail.com>
2026-08-21 10:37:11 +0530

    Time: 3 ms (66.67%), Space: 60.4 MB (17.95%) - LeetHub

--- 2706-buy-two-chocolates/2706-buy-two-chocolates.ts
+++ 2706-buy-two-chocolates/2706-buy-two-chocolates.ts
@@ -1,22 +1,8 @@
-function buyChoco(prices: number[], money: number): number {
-    let first = Infinity;
-    let second = Infinity;
-
-    for (let price of prices) {
-        if (price < first) {
-            second = first;
-            first = price;
-        } 
-        else if (price < second) {
-            second = price;
-        }
-    }
-
-    let total = first + second;
-
-    if (total <= money) {
-        return money - total;
-    }
-
-    return money;
+function buyChoco(price: number[], money: number): number {
+  price.sort((a,b)=>a-b)
+  let cost=price[0]+price[1]
+  if(cost<=money){
+    return money-cost
+  }
+  return money
 }
